1dadacfbfd7696446886e37f2a96be74521eb388
Rebecca Mooney <astrakiseki@gmail.com>
2016-04-12 08:02:33 -0700

    Adjustments and bower added among other things.

---
+++ MovieNerd/js/app.js
@@ -0,0 +1,18 @@
+﻿angular.module('app', []);
+
+angular.module('app').controller('indexController', function ($scope, $http) {
+
+    console.log('issuing http get');
+
+    $scope.searchTerm = '';
+
+    $http.get('http://www.omdbapi.com/?s=' + $scope.SearchTerm + '&page=2')
+.success(function (data) {
+    console.log(data);
+})
+.error(function (error) {
+    alert(error);
+}
+);
+
+});
